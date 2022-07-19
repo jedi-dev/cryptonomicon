910c82754e60f253dcbea08ad6171be938aa6d2c
jedi-dev <cityman177@gmail.com>
2022-07-19 15:01:31 +0300

    practice 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'cryptonomicon';
+  ticker: string = 'Crypto';
+  tickers = [
+    { name: 'BTC', price: 99999.99 },
+    { name: 'DOGE', price: 0.0014 },
+    { name: 'WTF', price: 1.11 },
+  ];
+  doubleTicker: boolean = false;
+  nameTickers: string[] = [];
+
+  searchDoubleTicker(): void {
+    if (this.tickers.length > 0) {
+      this.tickers.forEach((e) => {
+        this.nameTickers.push(e.name);
+      });
+    }
+    this.doubleTicker = this.nameTickers.includes(this.ticker.toUpperCase());
+  }
+
+  add(): void {
+    this.searchDoubleTicker();
+    if (!this.nameTickers.includes(this.ticker.toUpperCase())) {
+      this.tickers.push({ name: this.ticker.toUpperCase(), price: 1 });
+    }
+    this.ticker = '';
+  }
+
+  delete(name: string): void {
+    this.tickers = this.tickers.filter((e) => e.name !== name);
+  }
 }
